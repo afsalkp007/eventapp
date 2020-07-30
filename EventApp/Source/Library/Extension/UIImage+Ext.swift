09c8be77bf0cee3acc1e412b326bbed1ea8776e8
MohammedAfsal <afsalkp.mohammed@gmail.com>
2020-07-30 16:26:22 +0400

    added image extensioin

---
+++ EventApp/Source/Library/Extension/UIImage+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+Ext.swift
+//  EventApp
+//
+//  Created by Afsal's Macbook Pro on 30/07/2020.
+//  Copyright © 2020 Afsal. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIImage {
+    func sameAspectRatio(newHeight: CGFloat) -> UIImage {
+        let scale = newHeight / size.height
+        let newWidth = size.width * scale
+        let newSize = CGSize(width: newWidth, height: newHeight)
+        return UIGraphicsImageRenderer(size: newSize).image { _ in
+            self.draw(in: .init(origin: .zero, size: newSize))
+        }
+    }
+}
